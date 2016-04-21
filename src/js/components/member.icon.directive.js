5584a28eba4e0d58342e61c6bcc0a2c846162a7b
Danny Robinson <daniel@djrobinson.me>
2016-04-21 11:32:42 -0600

    Completed routing setup for a single user

---
+++ src/js/components/member.icon.directive.js
@@ -0,0 +1,32 @@
+(function(){
+  'use strict'
+
+  angular
+    .module('datingApp')
+    .directive('memberIcon', memberIcon)
+
+  function memberIcon(){
+    var directive = {
+      restrict: 'EA',
+      scope: {
+        id: '=',
+        avatar: '=',
+        username: '=',
+        first: '=',
+        last: '='
+      },
+      template: `
+                  <div class="col-md-12">
+                    <div class="col-md-4">
+                      <img ng-src="{{avatar}}" alt="profile_pic" />
+                    </div>
+                    <div class="col-md-8">
+                      <h4><a ui-sref="members.member({ member_id: id })">{{id}} {{username}}</a></h4>
+                      <h5>{{first}} {{last}}</h5>
+                    </div>
+                  </div>
+                `
+    }
+    return directive;
+  }
+})();
--- src/js/components/members.all.directive.js
+++ src/js/components/members.all.directive.js
@@ -14,19 +14,26 @@
                       <h2>Members All</h2>
                       <a ui-sref="members.search">Search</a>
                       <a ui-sref="members.member">Member</a>
+                      <div ng-repeat="member in vm.members">
+                        <member-icon
+                          id="member._id"
+                          avatar="member.avatar"
+                          username="member.username"
+                          first="member.names.firstName"
+                          last="member.names.lastName">
+                        </member-icon>
+                      </div>
                     </div>
                     <div class="col-md-8 main col-md-offset-4" ui-view></div>
                 `,
       controller: 'membersCtrl',
       controllerAs: 'vm'
-
     }
     return directive;
   }
 
   function membersCtrl($scope, memberService){
     var vm = this;
-
     vm.test = "hello";
     memberService.getMembers()
       .then(function(data){
@@ -34,6 +41,5 @@
         vm.members = data;
         return vm.members;
       });
-
   }
 })();
