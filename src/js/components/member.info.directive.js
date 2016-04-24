c08176e3c28dd656d9f2a7a21b2237108fa90686
Danny Robinson <daniel@djrobinson.me>
2016-04-23 19:29:06 -0600

    Completed initial member load distance error

--- src/js/components/member.info.directive.js
+++ src/js/components/member.info.directive.js
@@ -34,7 +34,7 @@
                           <h3>{{vm.selected.names.firstName}} {{vm.selected.names.lastName}}</h3>
                           <h3>{{vm.selected.email}}</h3>
                           <p>{{vm.selected.description}}</p>
-                          <p>{{vmInfo.distance}}</p>
+                          <p>{{vm.selected.distance}}</p>
 
 
                         </div>
@@ -50,31 +50,7 @@
 
   function infoCtrl($localStorage, $scope, matchService){
     var vmInfo = this;
-    var geo1 = $localStorage.geo;
-    var geo2 = $scope.vm.selected.address.geo;
 
-    function distance(lon1, lat1, lon2, lat2) {
-      var R = 6371; // Radius of the earth in km
-      var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
-      var dLon = (lon2-lon1).toRad();
-      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-              Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
-              Math.sin(dLon/2) * Math.sin(dLon/2);
-      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-      var d = R * c; // Distance in km
-      console.log("Distance!! ", d);
-      vmInfo.distance = d;
-      return d;
-    }
-
-    /** Converts numeric degrees to radians */
-    if (typeof(Number.prototype.toRad) === "undefined") {
-      Number.prototype.toRad = function() {
-        return this * Math.PI / 180;
-      }
-    }
-
-    var apart = distance(geo1.lng, geo1.lat, geo2.lng, geo2.lat);
 
     var member_id = $localStorage.user;
     vmInfo.createMatch = function(match){
--- src/js/components/members.all.directive.js
+++ src/js/components/members.all.directive.js
@@ -46,10 +46,39 @@
   function membersCtrl($scope, memberService, matchService, $localStorage){
     var vm = this;
 
+
+    function distance(lon1, lat1, lon2, lat2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = (lat2-lat1).toRad();  // Javascript functions in radians
+      var dLon = (lon2-lon1).toRad();
+      var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+              Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+              Math.sin(dLon/2) * Math.sin(dLon/2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+      var d = R * c; // Distance in km
+      console.log("Distance!! ", d);
+      return d;
+    }
+
+    /** Converts numeric degrees to radians */
+    if (typeof(Number.prototype.toRad) === "undefined") {
+      Number.prototype.toRad = function() {
+        return this * Math.PI / 180;
+      }
+    }
+
+
+
     vm.setSelected = function(id){
+
       if ( id ){
+
         memberService.getMember(id).then(function(data){
+          var geo1 = $localStorage.geo;
           vm.selected = data;
+          var geo2 = vm.selected.address.geo;
+          var apart = distance(geo1.lng, geo1.lat, geo2.lng, geo2.lat);
+          vm.selected.distance = apart;
         })
       }
     };
@@ -73,8 +102,6 @@
       vm.iterator = 0;
       vm.getPage();
     }
-
-    vm.testing = "bliggity blah";
 
     vm.memberSearch = function(newMembers){
       vm.members = newMembers;
